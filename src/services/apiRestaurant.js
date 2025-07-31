fa5cc999aa4c2c9e245681c2a0ea4b53c384427f
Zayed Mohammed Uddin <zayed.md.uddin273@gmail.com>
2025-07-31 18:37:56 -0400

    added try-catch

--- src/services/apiRestaurant.js
+++ src/services/apiRestaurant.js
@@ -1,51 +1,59 @@
 const API_URL = "https://react-fast-pizza-api.jonas.io/api";
 
 export async function getMenu() {
-	const res = await fetch(`${API_URL}/menu`);
-	if (!res.ok) throw Error("Failed getting menu");
+  try {
+    const res = await fetch(`${API_URL}/menu`);
+    if (!res.ok) throw Error("Failed getting menu");
 
-	const { data } = await res.json();
-	return data;
+    const { data } = await res.json();
+    return data;
+  } catch (err) {
+    throw Error(`Failed getting menu: ${err.message}`);
+  }
 }
 
 export async function getOrder(id) {
-	const res = await fetch(`${API_URL}/order/${id}`);
-	if (!res.ok) throw Error(`Couldn't find order #${id}`);
+  try {
+    const res = await fetch(`${API_URL}/order/${id}`);
+    if (!res.ok) throw Error(`Couldn't find order #${id}`);
 
-	const { data } = await res.json();
-	return data;
+    const { data } = await res.json();
+    return data;
+  } catch (err) {
+    throw Error(`Failed getting order: ${err.message}`);
+  }
 }
 
 export async function createOrder(newOrder) {
-	try {
-		const res = await fetch(`${API_URL}/order`, {
-			method: "POST",
-			body: JSON.stringify(newOrder),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
+  try {
+    const res = await fetch(`${API_URL}/order`, {
+      method: "POST",
+      body: JSON.stringify(newOrder),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-		if (!res.ok) throw Error();
-		const { data } = await res.json();
-		return data;
-	} catch {
-		throw Error("Failed creating your order");
-	}
+    if (!res.ok) throw Error();
+    const { data } = await res.json();
+    return data;
+  } catch {
+    throw Error("Failed creating your order");
+  }
 }
 
 export async function updateOrder(id, updateObj) {
-	try {
-		const res = await fetch(`${API_URL}/order/${id}`, {
-			method: "PATCH",
-			body: JSON.stringify(updateObj),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
+  try {
+    const res = await fetch(`${API_URL}/order/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify(updateObj),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-		if (!res.ok) throw Error();
-	} catch (err) {
-		throw Error(`Failed updating your order: ${err.message}`);
-	}
+    if (!res.ok) throw Error();
+  } catch (err) {
+    throw Error(`Failed updating your order: ${err.message}`);
+  }
 }
